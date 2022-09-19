44873cc8a56401603094da1e27792325effdf894
ranggawahyuds <ranggawahyuds@gmail.com>
2022-09-19 20:26:09 +0700

    tugas pertama

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -3,8 +3,8 @@ class contoh1 extends CI_Controller
 {
     public function index()
     {
-        echo"<h1>perkenalkan</h1>";
-        echo"Nama saya rangga wahyu firnanda
+        echo "<h1>perkenalkan</h1>";
+        echo "Nama saya rangga wahyu firnanda
              saya tinggal di daerah jeruju";
     }
 }
