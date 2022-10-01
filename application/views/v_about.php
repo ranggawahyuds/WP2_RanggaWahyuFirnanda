f84858c7bf2d2cb565d89d2e256b5e0805fca104
ranggawahyuds <ranggawahyuds@gmail.com>
2022-10-01 16:19:47 +0700

    tugas pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,21 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rangga Wahyu</li>
+        <li>Nama Belakang : Firnanda </li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl. Sapta Marga Gg. Hidayah No. B6</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Ketapang</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>-</li>
+    </ul>
+</section>
