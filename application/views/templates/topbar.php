f1ca1ae6968708d109e5652473477270506f5605
ranggawahyuds <ranggawahyuds@gmail.com>
2022-10-31 01:08:15 +0700

    revisi tugas 7

--- application/views/templates/topbar.php
+++ application/views/templates/topbar.php
@@ -1,25 +1,44 @@
-<?php 
-defined('BASEPATH') or exit('No direct script access allowed'); 
+<!-- Content Wrapper --> 
+<div id="content-wrapper" class="d-flex flex-column"> 
 
-class Admin extends CI_Controller 
-{ 
-    public function __construct() 
-    { 
-        parent::__construct(); 
-        cek_login(); 
-    } 
-    
-    public function index()
-    {
-        $data['judul'] = 'Dashboard'; 
-        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array(); 
-        $data['anggota'] = $this->ModelUser->getUserLimit()->result_array(); 
-        $data['buku'] = $this->ModelBuku->getBuku()->result_array(); 
-        
-        $this->load->view('templates/header', $data); 
-        $this->load->view('templates/sidebar', $data); 
-        $this->load->view('templates/topbar', $data); 
-        $this->load->view('admin/index', $data); 
-        $this->load->view('templates/footer'); 
-    } 
-}
+    <!-- Main Content --> 
+    <div id="content"> 
+
+        <!-- Topbar --> 
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow"> 
+
+            <!-- Sidebar Toggle (Topbar) --> 
+            <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3"> 
+                <i class="fa fa-bars"></i> 
+            </button> 
+
+            <!-- Page Heading --> 
+            <h1 class="h3 mb-2 text-gray-800"><?= $judul; ?></h1> 
+
+            <!-- Topbar Navbar --> 
+            <ul class="navbar-nav ml-auto"> 
+
+                <div class="topbar-divider d-none d-sm-block"></div> 
+
+                <!-- Nav Item - User Information --> 
+                <li class="nav-item dropdown no-arrow"> 
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> 
+                        <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $user['nama']; ?> 
+                        </span> 
+                        <img class="img-profile rounded-circle" src="<?= base_url('assets/img/profile/') . $user['image']; ?>"> 
+                    </a> 
+                    <!-- Dropdown - User Information --> 
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown"> 
+                        <a class="dropdown-item" href="<?= base_url('user'); ?>"> 
+                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>Profile Saya </a> 
+                        <div class="dropdown-divider"></div> 
+                        <a class="dropdown-item" href="<?= base_url('autentifikasi/logout'); ?>" data-toggle="modal" data-target="#logoutModal"> 
+                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>Logout 
+                        </a> 
+                    </div> 
+                </li> 
+
+            </ul> 
+
+        </nav> 
+        <!-- End of Topbar --> 
