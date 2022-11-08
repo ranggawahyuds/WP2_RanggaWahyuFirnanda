7273127d769d88c25c9f2bdc4dd714b0b98fc5fa
ranggawahyuds <ranggawahyuds@gmail.com>
2022-11-08 20:24:23 +0700

    tugas pertemuan 9

--- application/models/ModelUser.php
+++ application/models/ModelUser.php
@@ -6,20 +6,24 @@ class ModelUser extends CI_Model
     {
         $this->db->insert('user', $data);
     }
+
     public function cekData($where = null)
     {
         return $this->db->get_where('user', $where);
     }
+
     public function getUserWhere($where = null)
     {
         return $this->db->get_where('user', $where);
     }
+
     public function cekUserAccess($where = null)
     {
         $this->db->select('*');
         $this->db->from('access_menu');
         $this->db->where($where);
         return $this->db->get();
+    
     }
     public function getUserLimit()
     {
@@ -28,4 +32,4 @@ class ModelUser extends CI_Model
         $this->db->limit(10, 0);
         return $this->db->get();
     }
-    }
+}
